141da44601fc263858a0ba70d8ad619f61bd48db
tenthman <tenth.man@mail.com>
2024-06-27 15:31:38 +0300

    1

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+let card = document.getElementById('card');
+let cardHolder = document.getElementsByClassName('card-holder');
+let addBtn = document.getElementById('add-card-button');
 
-
-let box = document.getElementsByClassName('box');
-
+addBtn.onclick = function() {
+	let newCard = card.cloneNode(true);
+	newCard.hidden = false;
+	cardHolder[1].append(newCard);
+};
