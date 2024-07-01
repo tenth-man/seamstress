be21302ea05cf021349f32c74a0985c8559ca3ae
tenthman <tenth.man@mail.com>
2024-07-01 17:20:06 +0300

    1

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-let card = document.getElementById('card');
-let cardHolder = document.getElementsByClassName('card-holder');
-let addBtn = document.getElementById('add-card-button');
+let addBtn = document.getElementById('add-box-button');
+let container = document.getElementById('container');
+let wrapper = document.querySelectorAll('.wrapper');
+//let box = document.getElementById('box');
+let box = document.createElement('div');
+box.setAttribute('id','box');
+
+
+function minCh (parNode) {
+	let newMass = [];
+	for(let elem of parNode.children) {
+		newMass.push(elem.children.length);
+	}
+	//console.log(newMass);
+	return newMass.indexOf(Math.min(...newMass));
+}
 
 addBtn.onclick = function() {
-	let newCard = card.cloneNode(true);
-	newCard.hidden = false;
-	cardHolder[1].append(newCard);
+	console.log(minCh(container));
+	
+	//box.cloneNode(true);
+	//wrapper[minCh].append(newBox);
+	//container.children[minCh].append(newBox);
+	
 };
